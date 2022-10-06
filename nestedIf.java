b43ef5a0fdf8ece3488ea231054b345d119002b1
SUBRAT KUMAR NAYAK <79637254+subratkumarnayak02@users.noreply.github.com>
2022-10-06 23:52:01 +0530

    Add files via upload

---
+++ nestedIf.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class nestedIf{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in); 
+        System.out.println("Enter a Hardness: ");  
+        int h = sc.nextInt(); 
+        System.out.println("Enter a Carbon content: ");  
+        double c = sc.nextDouble();
+        System.out.println("Enter a Tensile strength: ");  
+        int t = sc.nextInt();
+        sc.close();
+
+        if (h>=50 && c<=0.7 && t>=5600) {
+            System.out.println("Grade is 10");
+        }
+        else if (h>=50 && c<=0.7) {
+            System.out.println("Grade is 9");
+        }
+        else if (c<=0.7 && t>=5600) {
+            System.out.println("Grade is 8");
+        }
+        else if (h>=50 && t>=5600) {
+            System.out.println("Grade is 7");
+        }
+        else if (h>=50 || c<=0.7 || t>=5600) {
+            System.out.println("Grade is 6");
+        }
+        else 
+            System.out.println("grade is 5");
+    }
+}
